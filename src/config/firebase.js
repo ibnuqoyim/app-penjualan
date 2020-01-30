016d3083131c7f2f185fa0d11a2e909d78f2e241
unknown <ibnuqoyim>
2020-01-31 05:36:49 +0700

    d

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDm3z5DyJ4AceM4g7ZVag8oRzJ9N13fxjo",
+    authDomain: "aplikasi-penjualan-b097e.firebaseapp.com",
+    databaseURL: "https://aplikasi-penjualan-b097e.firebaseio.com",
+    projectId: "aplikasi-penjualan-b097e",
+    storageBucket: "aplikasi-penjualan-b097e.appspot.com",
+    messagingSenderId: "774808681417",
+    appId: "1:774808681417:web:8fe60343746176ad2bc618",
+    measurementId: "G-V97ZXCKFF5"
+  };
+
+
+export default firebaseConfig;
